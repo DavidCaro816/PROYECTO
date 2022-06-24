39d8e92e17eb7815e82de365680be26cc04fb31f
Sebastian Alarcon Diaz <90793992+Sebas35@users.noreply.github.com>
2022-06-23 22:10:41 -0500

    Add files via upload

---
+++ IV Trimestre/Documentación version 4/Database/Schema/Tables/tables.sql
@@ -0,0 +1,212 @@
+drop database if exists Perez_Lara_Cia_LTDA;
+create database if not exists Perez_Lara_Cia_LTDA;
+use Perez_Lara_Cia_LTDA;
+
+create table if not exists tipo_documento
+(
+    id_tipo_documento int auto_increment primary key,
+    descripcion_documento varchar(45) not null,
+    abreviacion varchar(2)
+);
+
+/* Usuarios
+   =========== */
+create table if not exists tipo_usuario
+(
+    id_tipo_usuario int auto_increment primary key,
+    tipo_usuario tinyint(1) not null
+);
+
+create table if not exists estado_usuario
+(
+    id_estado_usuario int auto_increment primary key,
+    estado_usuario tinyint(1) not null
+);
+
+create table if not exists usuario
+(
+    documento int primary key,
+    primer_nombre varchar(45) not null,
+    segundo_nombre varchar(45),
+    primer_apellido varchar(45) not null,
+    segundo_apellido varchar(45),
+    celular varchar(15) not null,
+    email varchar(45) not null UNIQUE,
+    clave varchar(45) not null,
+    tipo_documento int not null,
+    tipo_usuario int not null,
+    estado_usuario int not null,
+    foreign key(tipo_documento) references tipo_documento(id_tipo_documento),
+    foreign key (tipo_usuario) references tipo_usuario(id_tipo_usuario),
+    foreign key (estado_usuario) references estado_usuario(id_estado_usuario)
+);
+
+create table if not exists notificacion
+(
+    id_notificacion int auto_increment primary key,
+    fecha datetime not null,
+    titulo varchar(45),
+    mensaje varchar(45)
+);
+
+create table if not exists estado_notificacion
+(
+    id_estado_notificacion int auto_increment primary key,
+    estado_notificacion varchar(45) not null
+);
+
+create table if not exists notificacionUsuario
+(
+    documento_usuario int not null,
+    notificacion int not null,
+    estado_notificacion int not null,
+    foreign key (documento_usuario) references usuario(documento),
+    foreign key (notificacion) references notificacion(id_notificacion),
+    foreign key (estado_notificacion) references estado_notificacion(id_estado_notificacion)
+);
+
+/* Clientes
+   =========== */
+create table if not exists departamento
+(
+    id_departamento int auto_increment primary key,
+    departamento varchar(60) not null
+);
+
+create table if not exists ciudad
+(
+    id_ciudad int auto_increment primary key,
+    ciudad varchar(60) not null,
+    departamento int not null,
+    foreign key(departamento) references departamento(id_departamento)
+);
+
+create table if not exists estado_cliente
+(
+    id_estado_cliente int auto_increment primary key,
+    estado_cliente varchar(45) not null
+);
+
+create table if not exists cliente
+(
+    documento int primary key,
+    primer_nombre varchar(45) not null,
+    segundo_nombre varchar(45),
+    primer_apellido varchar(45) not null,
+    segundo_apellido varchar(45),
+    fecha_nacimiento date not null,
+    email varchar(45) not null,
+    telefono1 varchar(15) not null,
+    telefono2 varchar(15),
+    direccion1 varchar(100) not null ,
+    direccion2 varchar(100),
+    tipo_documento int not null,
+    ciudad int not null,
+    estado_cliente int not null,
+    foreign key (tipo_documento) references tipo_documento(id_tipo_documento),
+    foreign key (ciudad) references ciudad(id_ciudad),
+    foreign key (estado_cliente) references estado_cliente(id_estado_cliente)
+);
+
+/* Productos
+   =========== */
+create table if not exists aseguradora
+(
+    id_aseguradora int auto_increment primary key,
+    aseguradora varchar(45) not null,
+    logo varchar(45) not null
+);
+
+create table if not exists seguro
+(
+    id_seguro int auto_increment primary key,
+    seguro varchar(45) not null,
+    imagen varchar(45) not null
+);
+
+create table if not exists seguro_aseguradora
+(
+    seguro int not null,
+    aseguradora int not null,
+    foreign key (seguro) references seguro(id_seguro),
+    foreign key (aseguradora) references aseguradora(id_aseguradora)
+);
+
+/* Trabajo
+   =========== */
+create table if not exists estado_cotizacion
+(
+    id_estado_cotizacion int auto_increment primary key,
+    estado_cotizacion varchar(45) not null
+);
+
+create table if not exists cotizacion
+(
+    id_cotizacion int auto_increment primary key,
+    documento_cliente int not null,
+    fecha date not null,
+    seguro int not null,
+    cuadro_comparativo varchar(45) not null,
+    estado_cotizacion int not null,
+    foreign key (documento_cliente) references cliente(documento),
+    foreign key (seguro) references seguro(id_seguro),
+    foreign key (estado_cotizacion) references estado_cotizacion(id_estado_cotizacion)
+);
+
+create table if not exists aseguradoraCotizante
+(
+    cotizacion int not null,
+    aseguradora int not null,
+    foreign key (cotizacion) references cotizacion(id_cotizacion),
+    foreign key (aseguradora) references aseguradora(id_aseguradora)
+);
+
+create table if not exists estado_poliza
+(
+    id_estado_poliza int auto_increment primary key,
+    estado_poliza varchar(45) not null
+);
+
+create table if not exists poliza
+(
+    codigo int auto_increment primary key,
+    documento_cliente int not null,
+    fecha date not null,
+    seguro int not null,
+    aseguradora int not null,
+    archivo varchar(45) not null,
+    estado_poliza int not null,
+    foreign key (documento_cliente) references cliente(documento),
+    foreign key (seguro) references seguro(id_seguro),
+    foreign key (aseguradora) references aseguradora(id_aseguradora),
+    foreign key (estado_poliza) references estado_poliza(id_estado_poliza)
+);
+
+create table if not exists estado_siniestro
+(
+    id_estado_siniestro int auto_increment primary key,
+    estado_siniestro varchar(45) not null
+);
+
+create table if not exists siniestro
+(
+    id_siniestro int auto_increment primary key,
+    fecha date not null,
+    descripcion varchar(45) not null,
+    monto DOUBLE,
+    codigo_poliza int not null,
+    anexo varchar(45) not null,
+    estado_siniestro int not null,
+    foreign key (codigo_poliza) references poliza(codigo),
+    foreign key (estado_siniestro) references estado_siniestro(id_estado_siniestro)
+);
+
+create table if not exists reporte
+(
+    idReporte int auto_increment primary key,
+    fecha date not null,
+    titulo varchar(45) not null,
+    archivo varchar(45) not null,
+    usuario int not null,
+    foreign key (usuario) references usuario(documento)
+);
---
+++ IV Trimestre/Documentación version 4/Database/Schema/Views/views.sql
@@ -0,0 +1,60 @@
+use Perez_Lara_Cia_LTDA;
+
+create view view_usuario as
+    select
+        concat(primer_nombre,' ',segundo_nombre,' ',primer_apellido,' ',segundo_apellido) as Nombre,
+        email as Email,celular as Celular,t2.tipo_usuario as Rol
+    from usuario
+        inner join tipo_documento t on usuario.tipo_documento = t.id_tipo_documento
+        inner join tipo_usuario t2 on usuario.tipo_usuario = t2.id_tipo_usuario
+        inner join estado_usuario e on usuario.estado_usuario = e.id_estado_usuario;
+
+create view view_cliente as
+    select
+        concat(t.abreviacion,' ',documento) as `No. Documento`,
+        concat(primer_nombre,' ',segundo_nombre,' ',primer_apellido,' ',segundo_apellido) as Cliente,
+        d.departamento as Departamento, c.ciudad as Ciudad, concat(direccion1,' ',direccion2) as Direccion,
+        fecha_nacimiento as `Fecha de Nacimiento`, email as Email, concat(telefono1,' ',telefono2) as Telefono, e.estado_cliente as Estado
+    from cliente
+        inner join tipo_documento t on cliente.tipo_documento = t.id_tipo_documento
+        inner join ciudad c on cliente.ciudad = c.id_ciudad
+        inner join departamento d on c.departamento = d.id_departamento
+        inner join estado_cliente e on cliente.estado_cliente = e.id_estado_cliente;
+
+create view view_cotizacion as
+    select
+        fecha as Fecha, id_cotizacion as `No. Cotizacion`, concat(c.primer_nombre,' ',c.segundo_nombre,' ',c.primer_apellido,' ',c.segundo_apellido) as Cliente,
+        s.seguro as Seguro, a2.aseguradora as Aseguradora, e.estado_cotizacion as Estado
+    from cotizacion
+        inner join cliente c on cotizacion.documento_cliente = c.documento
+        inner join seguro s on cotizacion.seguro = s.id_seguro
+        inner join aseguradoracotizante a on cotizacion.id_cotizacion = a.cotizacion
+        inner join aseguradora a2 on a.aseguradora = a2.id_aseguradora
+        inner join estado_cotizacion e on cotizacion.estado_cotizacion = e.id_estado_cotizacion;
+
+create view view_poliza as
+    select fecha as Fecha,codigo as `Codigo poliza`,
+           concat(primer_nombre,' ',segundo_nombre,' ',primer_apellido,' ',segundo_apellido) as Cliente,
+           s.seguro as Seguro,a.aseguradora as Aseguradora,e.estado_poliza as Estado
+    from poliza
+        inner join cliente c on poliza.documento_cliente = c.documento
+        inner join seguro s on poliza.seguro = s.id_seguro
+        inner join aseguradora a on poliza.aseguradora = a.id_aseguradora
+        inner join estado_poliza e on poliza.estado_poliza = e.id_estado_poliza;
+
+create view view_siniestro as
+    select id_siniestro as `No. Referencia`,
+           concat(primer_nombre,' ',segundo_nombre,' ',primer_apellido,' ',segundo_apellido) as Cliente,
+           codigo as `Codigo poliza`,p.seguro as Seguro,a.aseguradora as Aseguradora, siniestro.fecha as Fecha,
+           siniestro.estado_siniestro as Estado
+    from siniestro
+        inner join poliza p on siniestro.codigo_poliza = p.codigo
+        inner join seguro s on p.seguro = s.id_seguro
+        inner join aseguradora a on p.aseguradora = a.id_aseguradora
+        inner join cliente c on p.documento_cliente = c.documento
+        inner join estado_siniestro e on siniestro.estado_siniestro = e.id_estado_siniestro;
+
+create view view_reporte as
+    select idReporte as `No. Reporte`,fecha as Fecha,titulo as Nombre,archivo as `Reporte`,usuario as `Generado por`
+    from reporte
+        inner join usuario u on reporte.usuario = u.documento;
